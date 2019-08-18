aa0dd274f402babd8b5827e656bbbc1b57f23af0
marinhowrd <marinhowrd@gmail.com>
2019-08-18 16:17:24 -0700

    added forms for post submission and edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#@rmb18_2t5jx52=vz(ts@jc13e%@ocs59*4b^yl(+be-+7!f)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
